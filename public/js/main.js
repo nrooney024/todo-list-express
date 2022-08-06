59a0792e6bb1b558588d37fe0ea6a160b2dbd66c
nrooney024 <nrooney024@gmail.com>
2022-08-05 20:50:27 -0400

    Commented to-do list

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,10 @@
+// Initializing event listeners
+
 const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
+// Setting up event listeners to call each function at click
 Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteItem)
 })
@@ -14,58 +17,88 @@ Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
 
+// Deletes item from the database, it's an async function because it needs to wait until it gets the response from the server
 async function deleteItem(){
+    // Pulling what is being deleted
     const itemText = this.parentNode.childNodes[1].innerText
+    // In a try catch block in case it doesn't work
     try{
+        // Sending request to server through fetch
         const response = await fetch('deleteItem', {
+            // telling which CRUD method
             method: 'delete',
+            // Telling what kind of request file
             headers: {'Content-Type': 'application/json'},
+            // What's inside the request file 
             body: JSON.stringify({
               'itemFromJS': itemText
             })
           })
+        // Waiting for a response from the server
         const data = await response.json()
+        // The response from the server
         console.log(data)
+        // Reloading to show the updates
         location.reload()
-
+    // Logging any errors, if it doesn't work
     }catch(err){
         console.log(err)
     }
 }
 
+// Updating item as completed from the database, it's an async function because it needs to wait until it gets the response from the server
 async function markComplete(){
+    // pulling what's being set to complete
     const itemText = this.parentNode.childNodes[1].innerText
+    // In a try catch block in case it doesn't work
     try{
+        // Sending request to server through fetch
         const response = await fetch('markComplete', {
+            // telling which CRUD method
             method: 'put',
+            // Telling what kind of request file
             headers: {'Content-Type': 'application/json'},
+            // What's inside the request file 
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
           })
+        // Waiting for a response from the server
         const data = await response.json()
+        // The response from the server
         console.log(data)
+        // Reloading to show the updates
         location.reload()
-
+    // Logging any errors, if it doesn't work
     }catch(err){
         console.log(err)
     }
 }
 
+// Updating item as completed from the database, it's an async function because it needs to wait until it gets the response from the server
 async function markUnComplete(){
+    // pulling what's being set to incomplete
     const itemText = this.parentNode.childNodes[1].innerText
+    // In a try catch block in case it doesn't work
     try{
+        // Sending request to server through fetch
         const response = await fetch('markUnComplete', {
+            // telling which CRUD method
             method: 'put',
+            // Telling what kind of request file
             headers: {'Content-Type': 'application/json'},
+            // What's inside the request file 
             body: JSON.stringify({
                 'itemFromJS': itemText
             })
           })
+        // Waiting for a response from the server
         const data = await response.json()
+        // The response from the server
         console.log(data)
+        // Reloading to show the updates
         location.reload()
-
+    // Logging any errors, if it doesn't work
     }catch(err){
         console.log(err)
     }
--- server.js
+++ server.js
@@ -70,52 +70,71 @@ app.post('/addTodo', (request, response) => {
 })
 
 
-// Updating database
+// Updating todo item as complete
 app.put('/markComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        // Setting thing to complete
         $set: {
             completed: true
           }
     },{
+        // Sorting it in descending order
         sort: {_id: -1},
+
+        // If it doesn't already exist, don't add a new one
         upsert: false
     })
+    // What to do if promise is successful
     .then(result => {
+        // log complete and respond with the 'Mark Complete' json file
         console.log('Marked Complete')
         response.json('Marked Complete')
     })
+    // log the error if the promise isn't successful
     .catch(error => console.error(error))
 
 })
 
+// Updating the to-do item in the database as uncomplete
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        // Setting the item as not completed
         $set: {
             completed: false
           }
     },{
+        // Set in descending order
         sort: {_id: -1},
+
+        // If it's not already there, don't add it
         upsert: false
     })
+    // If the promise is successful, respond with a json file and console log that it's complete
     .then(result => {
         console.log('Marked Complete')
         response.json('Marked Complete')
     })
+    // if it's not successful, log the error
     .catch(error => console.error(error))
 
 })
 
-
+// Updating the item in the database as deleted
 app.delete('/deleteItem', (request, response) => {
+    // deleting item from database
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+    
+    // if the promise is successful, log and respond with a json file to let them know
     .then(result => {
         console.log('Todo Deleted')
         response.json('Todo Deleted')
     })
+    // if not, log the error
     .catch(error => console.error(error))
 
 })
 
+// Setting the listening port and letting them know which port it's in
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
